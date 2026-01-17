7d73083b94cdf76522f4427a9cf792bb294c541a
GiangPiano <truonggiang.piano@gmail.com>
2026-01-17 17:14:07 +1100

    Stop tracking build folder

--- build/CMakeFiles/fc-tmp/sfml/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("/Users/giang/OneDrive - UTS/Project/genetic-photo-recreation/build/CMakeFiles/fc-tmp/sfml/sfml-gitclone.cmake")
-
-endblock()
